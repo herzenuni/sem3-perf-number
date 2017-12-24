ec5f62fa4c3e299aa343a19a6ce1f5a9e8e230e9
Nick <monk11@ya.ru>
2017-12-24 19:01:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-
-is_perfect_number(number):
+def is_perfect_number(number):
     """
     docstring располагается здесь 
     """
-    
-    pass # ваш код располагается здесь
-    
+    pass  # ваш код располагается здесь
     # return True or False
-    
-    
+
+
 if __name__ == '__main__':
+    pass
     # тесты располагаются здесь
-    
-    
