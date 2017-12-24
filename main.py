02000ab6729ede5c27e191337b9f841e7421b804
Nick <monk11@ya.ru>
2017-12-24 14:28:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+is_perfect_number(number):
+    """
+    docstring располагается здесь 
+    """
+    
+    pass # ваш код располагается здесь
+    
+    # return True or False
+    
+    
+if __name__ == '__main__':
+    # тесты располагаются здесь
+    
+    
